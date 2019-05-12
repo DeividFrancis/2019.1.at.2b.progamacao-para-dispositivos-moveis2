e5da1713f3de42f18e4a91ded1f9d66035311266
DeividF <deividfrancisdeoliveira@gmail.com>
2019-05-11 23:33:59 -0400

    Tela de login, init

---
+++ app/src/main/java/com/github/deivifrancis/a20191at2bprogamacao_para_dispositivos_moveis/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.github.deivifrancis.a20191at2bprogamacao_para_dispositivos_moveis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
