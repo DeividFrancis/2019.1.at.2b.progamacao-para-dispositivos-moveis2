255b0a0bfbf6ad36781116657e8489c4ffe96c76
DeividF <deividfrancisdeoliveira@gmail.com>
2019-05-11 23:24:09 -0400

    first commit

---
+++ app/src/test/java/com/github/deivifrancis/a20191at2bprogamacao_para_dispositivos_moveis/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.github.deivifrancis.a20191at2bprogamacao_para_dispositivos_moveis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
